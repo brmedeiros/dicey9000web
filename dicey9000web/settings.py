eead3e3266ad43acbb09d342baf968bb4c80d2cf
brmedeiros <br.medeiros@gmail.com>
2017-01-26 19:59:43 -0300

    collects static files

--- dicey9000web/settings.py
+++ dicey9000web/settings.py
@@ -114,7 +114,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
