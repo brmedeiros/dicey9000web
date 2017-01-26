31a5e913a70d24535f5e60754bd0aacca2608ed9
brmedeiros <br.medeiros@gmail.com>
2017-01-26 19:24:13 -0300

    sets up venv, django and heroku

---
+++ dicey9000web/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dicey9000web project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dicey9000web.settings")
+
+application = get_wsgi_application()
