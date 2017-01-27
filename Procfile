c1c79f3f30e652554a6b51827ec440e2107df10e
brmedeiros <br.medeiros@gmail.com>
2017-01-26 23:38:43 -0300

    installs gunicorn for server handling
    * updates allowed servers in settings.py
    * updates requirements.txt
    * updates Procfile
    * updates .gitignore

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py runserver
+web: gunicorn dicey9000web.wsgi --log-file -
--- dicey9000web/settings.py
+++ dicey9000web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['DJANGO_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
