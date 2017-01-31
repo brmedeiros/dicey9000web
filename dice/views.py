4abc832fc789b3192273bc872a2872220f108c1a
brmedeiros <br.medeiros@gmail.com>
2017-01-31 00:20:27 -0300

    adds minor adjustments to views.py in the dice app

--- dice/views.py
+++ dice/views.py
@@ -11,19 +11,17 @@ def home(request):
         form = HomeDiceRollForm(request.POST)
         roll_output = ''
         if form.is_valid():
-            print('form ok')
             number_of_dice = form.clean_number_of_dice()
             dice_type = form.clean_dice_type()
             roll_modifier = form.clean_roll_modifier()
             explode_value = form.clean_explode_value()
             success_condition = form.clean_success_condition()
-            roll = DiceRoll(number_of_dice, dice_type, roll_modifier,
+            roll = DiceRoll(number_of_dice, dice_type, roll_modifier, 
                             explode_value, success_condition)
             roll.roll_dice()
             roll.explode_dice()
             roll.success_counter()
             roll_output = roll.output()
-            print(roll_output)
         return render(request, 'dice/home.html', {
             'form': form,
             'roll_output': roll_output,
